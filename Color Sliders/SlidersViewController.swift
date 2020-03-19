7b6de52340f5ffe1d1264839e9b6e994bfc5e75a
David Fisher <fisherds@rose-hulman.edu>
2020-03-19 12:57:19 -0400

    finished my version

--- Color Sliders/SlidersViewController.swift
+++ Color Sliders/SlidersViewController.swift
@@ -11,9 +11,17 @@ import UIKit
 
 class SlidersViewController: UIViewController {
 
-  @IBOutlet weak var redLabel: UILabel!
-  @IBOutlet weak var redSlider: UISlider!
 
+  @IBOutlet weak var redSlider: UISlider!
+  @IBOutlet weak var greenSlider: UISlider!
+  @IBOutlet weak var blueSlider: UISlider!
+  @IBOutlet weak var alphaSlider: UISlider!
+  @IBOutlet weak var redLabel: UILabel!
+  @IBOutlet weak var greenLabel: UILabel!
+  @IBOutlet weak var blueLabel: UILabel!
+  @IBOutlet weak var alphaLabel: UILabel!
+
+  @IBOutlet weak var colorView: UIView!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
@@ -22,7 +30,19 @@ class SlidersViewController: UIViewController {
 
   @IBAction func sliderChanged(_ sender: Any) {
 //    redLabel.text = "\(redSlider.value)"
+    updateView()
+  }
+  func updateView() {
     redLabel.text = String(format: "%.2f", redSlider.value)
-    
+    greenLabel.text = String(format: "%.2f", greenSlider.value)
+    blueLabel.text = String(format: "%.2f", blueSlider.value)
+    alphaLabel.text = String(format: "%.2f", alphaSlider.value)
+
+    colorView.backgroundColor = UIColor(
+      red: CGFloat(redSlider.value),
+      green: CGFloat(greenSlider.value),
+      blue: CGFloat(blueSlider.value),
+      alpha: CGFloat(alphaSlider.value))
+
   }
 }
